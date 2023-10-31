e6b507144be99dc82d8d73c791af43228bbda8fc
AsyaTanis <tasyaosipova736@gmail.com>
2023-10-31 12:09:36 +0300

    Курсовая работа

---
+++ src/main/java/com/skypro/simplebanking/repository/AccountRepository.java
@@ -0,0 +1,18 @@
+package com.skypro.simplebanking.repository;
+
+import com.skypro.simplebanking.entity.Account;
+
+import java.util.Collection;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+  Optional<Account> getAccountByUser_IdAndId(Long userId, Long accountId);
+
+  Collection<Account> findByUserId(long userId);
+
+}
+
+
